b0e9d2f769b7bca720f0cff11c0e77d9b1b82193
Nico Majander <peacemake@gmail.com>
2018-11-04 20:32:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Ledger - Kill Team team management
+
+Mainly done for my personal amusement and a motivating goal to learn React with.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ src/teams.js
@@ -0,0 +1,39 @@
+const teams = [
+  {
+    "name": "Auringonlaskun ratsastajat",
+    "type": "Matched Play",
+    "pointLimit": 100,
+    "faction": "Astra Militarum",
+    "members": [
+      {
+        "type": "Leader",
+        "name": "Jaska",
+        "class": "Special Weapons Guardsman",
+        "equipment": [
+          "Lasgun",
+          "Frag Grenades"
+        ]
+      },
+      {
+        "type": "Scout",
+        "name": "Esko",
+        "class": "Special Weapons Guardsman",
+        "equipment": [
+          "Flamer",
+          "Frag Grenades"
+        ]
+      },
+      {
+        "type": "Veteran",
+        "name": "Markku",
+        "class": "Scion",
+        "equipment": [
+          "Meltagun",
+          "Frag Grenades"
+        ]
+      }
+    ]
+  }
+]
+
+export default teams
